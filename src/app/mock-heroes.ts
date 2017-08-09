03bc5da3a70c4d1a16988cce21fe7e0d96e10ac3
Akash <akash.kumar03@gmail.com>
2017-08-09 12:04:43 +0530

    Added my first angular demo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+ 
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Spider Man'},
+  { id: 12, name: 'Swat Cats'},
+  { id: 13, name: 'The Hulk'},
+  { id: 14, name: 'Iron Man'},
+  { id: 15, name: 'Wolverine'},
+  { id: 16, name: 'Ant Man'},
+  { id: 17, name: 'Thor'},
+  { id: 18, name: 'Captain America'},
+  { id: 19, name: 'Dr. Octopus'},
+  { id: 20, name: 'Invisible Man'}
+];
